296fa81a78af1064a6a359d38a5ce58ee2eb47c6
snipeslow <38489015+snipeslow@users.noreply.github.com>
2024-02-09 22:09:35 -0600

    Added automatic link to CombatSystem

---
+++ CombatLink/Scripts/CombatLink_CombatSystemWrapper.cs
@@ -0,0 +1,30 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class CombatLink_CombatSystemWrapper : UdonSharpBehaviour
+{
+    public CombatLinkManager CombatLinkManager;
+    [Header("CombatLink variables below!")]
+    public bool Active;
+    float MaxHealth = 0;
+    private void LateUpdate()
+    {
+        if (CombatLinkManager)
+        {
+            CombatLinkManager.Active = Active;
+            CombatLinkManager.Health = Networking.LocalPlayer.CombatGetCurrentHitpoints();
+            if(MaxHealth > CombatLinkManager.Health)
+            {
+                MaxHealth = CombatLinkManager.Health;
+            }
+            CombatLinkManager.MaxHealth = MaxHealth;
+        }
+    }
+    private void OnDisable()
+    {
+        MaxHealth = 0;
+    }
+}
